1a0cfe830c307bd44afc69d3bcb224c2df7fb585
Mattwmaster58 <mattmarcus58@gmail.com>
2022-01-01 19:23:18 -0500

    actually dedent properly

--- BetterCodeblocks/BetterCodeblocks.plugin.js
+++ BetterCodeblocks/BetterCodeblocks.plugin.js
@@ -309,49 +309,22 @@ module.exports = (() => {
 
 
     dedent(strings) {
-      // from https://github.com/dmnd/dedent/
-      var raw = typeof strings === "string" ? [strings] : strings.raw;
-
-      // first, perform interpolation
-      var result = "";
-      for (var i = 0; i < raw.length; i++) {
-        result += raw[i].
-          // join lines when there is a suppressed newline
-          replace(/\\\n[ \t]*/g, "").
-
-          // handle escaped backticks
-          replace(/\\`/g, "`");
-
-        if (i < (arguments.length <= 1 ? 0 : arguments.length - 1)) {
-          result += arguments.length <= i + 1 ? undefined : arguments[i + 1];
-        }
-      }
-
-      // now strip indentation
-      var lines = result.split("\n");
-      var mindent = null;
-      lines.forEach(function (l) {
-        var m = l.match(/^(\s+)\S+/);
-        if (m) {
-          var indent = m[1].length;
-          if (!mindent) {
-            // this is the first indented line
-            mindent = indent;
-          } else {
-            mindent = Math.min(mindent, indent);
-          }
-        }
-      });
-
-      if (mindent !== null) {
-        (function () {
-          var m = mindent; // appease Flow
-          result = lines.map(function (l) {
-            return l[0] === " " ? l.slice(m) : l;
-          }).join("\n");
-        })();
-      }
-      return result
+      // from https://github.com/brandonkal/dedent-js/tree/patch-1
+			// 1. Remove trailing whitespace.
+			input = input.replace(/\r?\n([\t ]*)$/, '');
+			// 2. Find all line breaks to determine the highest common indentation level.
+			let match = null;
+			if ((match = input.match(/\n[\t ]+/g))) {
+			} else if (input.match(/\n[\w\d-]/g)) {
+				match = {length: 1};
+			}
+			// 4. Remove the common indentation from all strings.
+			if (match) {
+				let pattern = new RegExp(`\n[\t ]{${match.length - 1}}`, 'g');
+				input.replace(pattern, '\n');
+			}
+			// 5. Remove leading whitespace.
+			return input.replace(/^\r?\n/, '');
     }
 
 		inject(nodes, output) {
