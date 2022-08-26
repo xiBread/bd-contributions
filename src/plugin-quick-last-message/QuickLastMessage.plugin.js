209a0b6e7a2c1f7bb56be16539bc37d0744d057a
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-08-26 13:49:35 -0400

    refactor(QLM): rewrite (still broken)

--- src/plugin-quick-last-message/QuickLastMessage.plugin.js
+++ src/plugin-quick-last-message/QuickLastMessage.plugin.js
@@ -2,130 +2,49 @@
  * @name QuickLastMessage
  * @author Bread
  * @authorId 304260051915374603
- * @website https://github.com/xiBread/bd-contributions/tree/master/QuickLastMessage
- * @source https://raw.githubusercontent.com/xiBread/bd-contributions/master/QuickLastMessage/QuickLastMessage.plugin.js
- * @updateUrl https://raw.githubusercontent.com/xiBread/bd-contributions/master/QuickLastMessage/QuickLastMessage.plugin.js
+ * @description Quickly access your last message by pressing the down arrow
+ * @version 1.0.7
+ * @website https://github.com/xiBread/bd-contributions
  */
 
- module.exports = (() => {
-	const config = {
-		info: {
-			name: "QuickLastMessage",
-			authors: [{ name: "Bread", discord_id: "304260051915374603", github_username: "xiBread" }],
-			version: "1.0.6",
-			description: "Quickly access your last message by pressing the down arrow",
-			github: "https://github.com/xiBread/bd-contributions/tree/master/QuickLastMessage",
-			github_raw:
-				"https://raw.githubusercontent.com/xiBread/bd-contributions/master/QuickLastMessage/QuickLastMessage.plugin.js",
-		},
-		changelog: [
-			{
-				title: "Works again",
-				type: "fixed",
-				items: [""]
+module.exports = () => ({
+	active: true,
+	start() {
+		const webpack = BdApi.Webpack;
+		const byProps = webpack.Filters.byProps;
+
+		const { getMessages } = webpack.getModule(byProps("getMessage", "getMessages"));
+		const { getCurrentUser } = webpack.getModule(byProps("getCurrentUser", "getUser"));
+
+		const { getChannelId } = webpack.getModule(byProps("getLastSelectedChannelId"));
+		const { ComponentDispatch } = webpack.getModule(byProps("ComponentDispatch"));
+
+		document.addEventListener("keydown", (event) => {
+			if (!this.active || event.ctrlKey || event.altKey) return;
+
+			if (event.key === "ArrowDown") {
+				const lastMsg = getMessages(getChannelId())
+					.toArray()
+					.filter((msg) => msg.content.trim() && msg.author.id === getCurrentUser().id)
+					.pop();
+
+				const [placeholder, textArea] = document.querySelectorAll(
+					"div[class*='slateTextArea']"
+				);
+
+				if (!lastMsg || document.activeElement !== textArea || !placeholder) {
+					return;
+				}
+
+				// FIXME
+				ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
+					content: lastMsg.content.trim()
+				});
 			}
-		],
-	};
+		});
+	},
 
-	return !global.ZeresPluginLibrary
-		? class {
-				constructor() {
-					this._config = config;
-				}
-				getName() {
-					return config.info.name;
-				}
-				getAuthor() {
-					return config.info.authors.map((a) => a.name).join(", ");
-				}
-				getDescription() {
-					return config.info.description;
-				}
-				getVersion() {
-					return config.info.version;
-				}
-				load() {
-					BdApi.showConfirmationModal(
-						"Library Missing",
-						`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
-						{
-							confirmText: "Download Now",
-							cancelText: "Cancel",
-							onConfirm: () => {
-								require("request").get(
-									"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-									async (error, response, body) => {
-										if (error)
-											return require("electron").shell.openExternal(
-												"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
-											);
-										await new Promise((r) =>
-											require("fs").writeFile(
-												require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"),
-												body,
-												r
-											)
-										);
-									}
-								);
-							},
-						}
-					);
-				}
-				start() {}
-				stop() {}
-		  }
-		: (([Plugin, Api]) => {
-				const plugin = (Plugin, Library) => {
-					const { WebpackModules, DiscordModules } = Library;
-
-					return class QuickLastMessage extends Plugin {
-						constructor() {
-							super();
-
-							this.active = true;
-						}
-
-						onStart() {
-							const { getMessages } = DiscordModules.MessageStore;
-							const { getCurrentUser } = DiscordModules.UserStore;
-
-							const { getChannelId } = WebpackModules.getByProps("getLastSelectedChannelId");
-							const { ComponentDispatch } = WebpackModules.getByProps("ComponentDispatch");
-
-							document.addEventListener("keydown", (event) => {
-								if (!this.active || event.ctrlKey || event.altKey) return;
-
-								if (event.key === "ArrowDown") {
-									let message;
-
-									getMessages(getChannelId())
-										.toArray()
-										.map((msg) => {
-											if (msg.content.trim() && msg.author.id === getCurrentUser().id) {
-												message = msg;
-											}
-										});
-
-									const { textContent } =
-										document.querySelector("div[class*='slateTextArea']").childNodes[0]
-											.childNodes[0];
-									const role = document.activeElement.getAttribute("role");
-
-									if (!textContent.trim().length && message && role === "textbox") {
-										ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
-											content: message.content.trim(),
-										});
-									}
-								}
-							});
-						}
-
-						onStop() {
-							this.active = false;
-						}
-					};
-				};
-				return plugin(Plugin, Api);
-		  })(global.ZeresPluginLibrary.buildPlugin(config));
-})();
+	stop() {
+		this.active = false;
+	},
+});
